fa383a07370500e3856c92481ce36cdfa886688c
rifkiedr <rifkiedr@gmail.com>
2023-01-13 07:54:53 +0700

    countries

---
+++ countries/countries.js
@@ -0,0 +1,51 @@
+// Read in the JSON files using fetch
+Promise.all([
+    fetch('countries.json').then(response => response.json()),
+    fetch('states.json').then(response => response.json()),
+    fetch('cities.json').then(response => response.json())
+  ]).then(([countries, states, cities]) => {
+      //populate the countries select field
+      const countrySelect = document.getElementById('country');
+      for (const country of countries) {
+          const option = document.createElement('option');
+          option.value = country.name;
+          option.innerHTML = `${country.emoji} ${country.name}`;
+          countrySelect.appendChild(option);
+      }
+      //add event listener to country select field
+      countrySelect.addEventListener('change', (event) => {
+          const selectedCountry = event.target.value;
+          //populate the states select field
+          const stateSelect = document.getElementById('state');
+          stateSelect.innerHTML = '';
+          stateSelect.disabled = false;
+          for (const state of states) {
+              if (state.country === selectedCountry) {
+                  const option = document.createElement('option');
+                  option.value = state.name;
+                  option.innerHTML = state.name;
+                  stateSelect.appendChild(option);
+              }
+          }
+          //populate the cities select field
+          const citySelect = document.getElementById('city');
+          citySelect.innerHTML = '';
+          citySelect.disabled = true;
+      });
+      //add event listener to state select field
+      stateSelect.addEventListener('change', (event) => {
+          const selectedState = event.target.value;
+          //populate the cities select field
+          const citySelect = document.getElementById('city');
+          citySelect.innerHTML = '';
+          citySelect.disabled = false;
+          for (const city of cities) {
+              if (city.state === selectedState) {
+                  const option = document.createElement('option');
+                  option.value = city.name;
+                  option.innerHTML = city.name;
+                  citySelect.appendChild(option);
+              }
+          }
+      });
+  });
