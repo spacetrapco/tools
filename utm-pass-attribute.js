9d0b2c36f41ef7575c555603733d09bb0d9fd0ea
Spacetrap Studio <119510419+spacetrapco@users.noreply.github.com>
2023-01-26 09:49:00 +0700

    utm-pass-attribute

---
+++ utm-pass-attribute.js
@@ -0,0 +1,17 @@
+// Get all buttons that have the attribute "tracking=utm-source"
+var buttons = document.querySelectorAll('button[tracking="utm-source"]');
+
+// Get the current URL
+var currentUrl = window.location.href;
+
+// Check if the URL contains the utm_source parameter
+if (currentUrl.indexOf("utm_source=") !== -1) {
+  // Extract the value of the utm_source parameter
+  var utmSource = currentUrl.split("utm_source=")[1];
+
+  // Iterate over all buttons
+  buttons.forEach(function(button) {
+    // Replace the text "Google" with the value of the utm_source parameter
+    button.href = button.href.replace("Google", utmSource);
+  });
+}
